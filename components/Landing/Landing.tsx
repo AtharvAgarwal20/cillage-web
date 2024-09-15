f9e0eb98dc2aaa7f5425c6e1131a41435b5569d5
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-09-15 17:33:58 +0530

    feat: added video

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -1,5 +1,18 @@
 import styles from "./landing.module.scss";
 
 export default function Landing() {
-  return <main className={styles.container}></main>;
+  return (
+    <main className={styles.container}>
+      <section className={styles.content}>
+        <iframe
+          className={styles.video}
+          src="https://www.youtube.com/embed/3DWBOwaohFc"
+          title="My Village - Cinematic Video | Sigma 30mm 1.4 | Sony a6400"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        />
+      </section>
+    </main>
+  );
 }
