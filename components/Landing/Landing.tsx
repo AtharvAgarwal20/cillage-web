e9cf60fddbe16243f6e9af8d009ac6b23032c3bc
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-09-15 18:05:52 +0530

    feat: added back the reverted changes

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -12,7 +12,12 @@ export default function Landing() {
           referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
         />
+        <h1 className={styles.tagline}>Transforming Village Life</h1>
       </section>
+      <div className={styles.footer}>
+        <div className={styles.villager}></div>
+        <div className={styles.wheat}></div>
+      </div>
     </main>
   );
 }
