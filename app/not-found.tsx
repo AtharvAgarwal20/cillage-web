69106bbe4f6086069877d9111c60d9a20b9a804a
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-09-15 16:04:10 +0530

    feat: created a not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+export default function NotFoundPage() {
+  return (
+    <main
+      style={{
+        width: "100vw",
+        height: "100dvh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1 style={{ color: "#e0961d", fontSize: "64px" }}>
+        404 | Page Not Found
+      </h1>
+    </main>
+  );
+}
