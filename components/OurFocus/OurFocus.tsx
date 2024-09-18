ef61e30145d0c3ce33f7fde801f169ff46badf3b
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-09-18 17:11:01 +0530

    adding the embala carousel

--- components/OurFocus/OurFocus.tsx
+++ components/OurFocus/OurFocus.tsx
@@ -1,12 +1,19 @@
 import Carousel from "./Carousel/Carousel";
+import EmblaCarousel from "./EmblaCarousel/EmblaCarousel";
 import styles from "./ourfocus.module.scss";
+import { EmblaOptionsType } from "embla-carousel";
+
+const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true };
+const SLIDE_COUNT = 5;
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 export default function OurFocus() {
   return (
     <main className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Our Focus</h1>
-        <Carousel />
+        {/* <Carousel /> */}
+        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
       </div>
     </main>
   );
